b3c246e67f6306d7dda65348008a3b943cc5f45b
AshutoshChaurasiya <ac.ch503@gmail.com>
2019-04-01 11:30:08 +0545

    loading button

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private RadioButton rdopit, rdohamal, rdorai;
+    private ImageView imgview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rdopit = findViewById(R.id.rdopit);
+        rdohamal = findViewById(R.id.rdohmal);
+        rdorai = findViewById(R.id.rdorai);
+
+        rdopit.setOnClickListener(this );
+        rdohamal.setOnClickListener(this);
+        rdorai.setOnClickListener(this);
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rdopit:
+                Toast toast = Toast.makeText(this, "pit", Toast.LENGTH_LONG);
+                toast.show();
+                break;
+        }
+
+        switch (v.getId()) {
+            case R.id.rdohmal:
+                Toast toast = Toast.makeText(this, "hamal", Toast.LENGTH_LONG);
+                toast.show();
+                break;
+        }
+
+        switch (v.getId()) {
+            case R.id.rdorai:
+                Toast toast = Toast.makeText(this, "rai", Toast.LENGTH_LONG);
+                toast.show();
+                break;
+        }
     }
 }
